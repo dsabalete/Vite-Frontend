e94e2c2f442cbe6d12c9c13f2fba2f86e0eaaf8e
David Sabalete Rodríguez <dsabalete@gmail.com>
2022-10-04 23:19:04 +0200

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite!!!" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite!!!" /> -->
+  <div class="container bg-green-500">
+    <div class="row">
+      <div class="col">
+        <h1>Vue 3 + Vite + Tailwind CSS</h1>
+        <p>
+          <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Documentation
+          </a>
+          |
+          <a href="https://v3.vuejs.org/" target="_blank"
+            >Vue 3 Documentation</a
+          >
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
